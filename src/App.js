218524e8d5aa0f6575686d9f5fe4fa88c0192d38
Rafia Khandaker <rafiakhandaker@gmail.com>
2023-02-01 19:56:59 -0500

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Resume from './Resume';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
+    // <BrowserRouter>
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <p>homepage</p>
         </p>
         <a
           className="App-link"
@@ -18,8 +23,21 @@ function App() {
           Learn React
         </a>
       </header>
+
+<Router>
+        <Switch>
+          {/* <Route exact path="/">
+          </Route> */}
+          <Route exact path="/resume">
+            <Resume />
+          </Route>
+        </Switch>
+        </Router>
+
     </div>
+    // </BrowserRouter>
   );
+  
 }
 
 export default App;
---
+++ src/Resume.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Resume() {
+  return (
+    <div className="Resume">
+      <h1>Resume Page</h1>
+    </div>
+  );
+}
+
+export default Resume;
