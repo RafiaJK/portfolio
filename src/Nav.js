9fa0e1137758d94cda70c379cd95dcf67d9ccd3e
Rafia Khandaker <rafiakhandaker@gmail.com>
2023-02-23 23:33:00 -0500

    Started navbar styling

--- src/Nav.js
+++ src/Nav.js
@@ -1,17 +1,33 @@
 import './App.css';
+import './nav.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Nav() {
+  const [showNav, setShowNav] = useState(false)
+
+  const toggleNav = () => {
+    setShowNav(!showNav)
+  }
+
   return (
-    <div className="Nav">
+    <div className="nav">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
       <nav className='navbar'>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About Rafia</Link></li>
-          <li><Link to="/resume">Resume</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-        </ul>
+
+      <div className="logo">
+        <Link to="/" >ADD LOGO/HOME</Link>
+      </div>
+
+        <div className="navlinks">
+          <ul>
+            <li><Link to="/about">About Rafia</Link></li>
+            <li><Link to="/resume">Resume</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+          </ul>
+        </div>
+
       </nav>
 
     </div>
