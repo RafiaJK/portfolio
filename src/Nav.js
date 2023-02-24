7a1d5c4ac0b0472ede6ef01648ca7edc6b58da9e
Rafia Khandaker <rafiakhandaker@gmail.com>
2023-02-24 00:27:51 -0500

    Functional nav across desktop and mobile

--- src/Nav.js
+++ src/Nav.js
@@ -12,22 +12,24 @@ function Nav() {
 
   return (
     <div className="nav">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-
       <nav className='navbar'>
-
+      <div className='nav-container'>
       <div className="logo">
         <Link to="/" >ADD LOGO/HOME</Link>
       </div>
 
-        <div className="navlinks">
+      <div className="menu-icon" onClick={toggleNav}>
+          <Link to="/" >logo</Link>
+      </div>
+
+        <div className={`navlinks ${showNav && 'active'}`}>
           <ul>
             <li><Link to="/about">About Rafia</Link></li>
             <li><Link to="/resume">Resume</Link></li>
             <li><Link to="/projects">Projects</Link></li>
           </ul>
         </div>
-
+        </div>
       </nav>
 
     </div>
