53522ca72ae1178d0ebc4a30899b3e245f74e3af
Rafia Khandaker <rafiakhandaker@gmail.com>
2023-12-28 01:51:30 -0500

    reformatted home page image

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import './App.css';
 import './Home.css';
 import profpic from './profpic.png'
+import profpic2 from './profpic2.jpg'
+
 import { TypeAnimation } from 'react-type-animation';
 import CodingTimeline from './CodingTimeline';
 import cityscape from './cityscape.jpg'
@@ -31,7 +33,7 @@ function Home() {
 
       <div className='home-images-block'>
         <img src={cityscape} alt="nyc" className='nyc'></img>
-        <img src={profpic} alt="photo of Rafia" className='profilepic'></img>
+        <img src={profpic2} alt="photo of Rafia" className='profilepic'></img>
         <h2 className='newyorkpin'>📍 New York</h2>
       </div>
 
